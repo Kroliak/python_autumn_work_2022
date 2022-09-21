2803e4c5720181b5f929151325aa3d7fd97fcc5a
Kroliak <denis.nikolashin@gmail.com>
2022-09-21 12:17:17 +0300

    Пробное

---
+++ python_autumn_work_2022/unit_one/task1.py
@@ -0,0 +1,16 @@
+
+x = int(input("Введите целое число: "))
+if x > 0:
+    print(x+1)
+elif x < 0:
+    print(x-2)
+else:
+    print(10)
+
+y = int(input("Введите целое число от 10 до 99: "))
+if 99 >= y >= 10:
+    print(str (y)
+    else
+
+
+
