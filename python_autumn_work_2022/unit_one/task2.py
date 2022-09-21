4590dcac00e7e2c0ac532228a047acc47190960b
Kroliak <denis.nikolashin@gmail.com>
2022-09-21 14:24:28 +0300

    Первое зданание

---
+++ python_autumn_work_2022/unit_one/task2.py
@@ -0,0 +1,11 @@
+#Преобразуйте переменную age и foo в число 
+age = "23"
+foo = "23abc"
+#Преобразуйте переменную age в Boolean
+age = 123abc
+#Преобразуйте переменную flag в Boolean
+flag = 1
+#Преобразуйте значение  в Boolean
+str_one = "Privet"
+str_two = ""
+#Преобразуйте значение 0 и 1  в Boolean
---
+++ python_autumn_work_2022/unit_one/task4.py
@@ -0,0 +1,7 @@
+# todo:  Создайте переменные, что бы вывести требуемый текст.
+
+# задан нерабочий код
+# требуемый вывод:
+# Мой стек: python, javascript, php
+
+print("Мой стек:", programming_lang_1, programming lang_2, programming_lang_3)
